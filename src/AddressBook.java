1586605a853ac2bbee15489ed6a8e6f6029d2cc7
oolongteaa <61853030+oolongteaa@users.noreply.github.com>
2021-09-23 19:12:17 -0400

    Update AddressBook.java

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -27,5 +27,6 @@ public class AddressBook {
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(buddy);
+        System.out.println("test");
     }
 }
